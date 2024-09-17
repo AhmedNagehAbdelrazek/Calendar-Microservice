63fcbdd813214f10f611a44086aa3094430a02fd
MoGamal27 <113600854+MoGamal27@users.noreply.github.com>
2024-09-17 18:58:54 +0300

    feat(calender): add calender controller

---
+++ Controller/calenderController.js
@@ -0,0 +1,59 @@
+// controllers/calendarController.js
+const CalendarEvent = require('../Model/calenderModel');
+const User = require('../Model/userModel');
+const googleCalendarService = require('../Service/googleCalenderService');
+
+
+/**
+ * Adds a new calendar event to the local database and synchronizes it with the user's Google Calendar.
+ *
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} req.body - The request body containing the event details.
+ * @param {string} req.body.title - The title of the event.
+ * @param {string} req.body.description - The description of the event.
+ * @param {string} req.body.startDateTime - The start date and time of the event.
+ * @param {string} req.body.endDateTime - The end date and time of the event.
+ * @param {string} req.body.location - The location of the event.
+ * @param {string} req.body.userId - The ID of the user creating the event.
+ * @param {Object} res - The HTTP response object.
+ * @returns {Promise<Object>} - The newly created calendar event.
+ */
+exports.addEvent = async (req, res) => {
+  try {
+    const { title, description, startDateTime, endDateTime, location, userId } = req.body;
+
+    // Create event in local database
+    const newEvent = new CalendarEvent({
+      title,
+      description,
+      startDateTime,
+      endDateTime,
+      location,
+      userId
+    });
+    await newEvent.save();
+
+    // Add event to Google Calendar
+    const googleEvent = {
+      summary: title,
+      description,
+      start: { dateTime: startDateTime },
+      end: { dateTime: endDateTime },
+      location
+    };
+    const result = await googleCalendarService.addEvent(userId, 'primary', googleEvent);
+
+    // Update local event with Google Calendar ID
+    newEvent.googleCalendarEventId = result.id;
+    newEvent.isSync = true;
+    await newEvent.save();
+
+    res.json(newEvent);
+  } catch (error) {
+    console.log('Error adding event to Google Calendar:', error);
+    res.status(500).json({ error: 'Failed to add event' });
+  }
+};
+
+
+
