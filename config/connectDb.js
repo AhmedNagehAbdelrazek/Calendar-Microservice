d47cf263c256f75ce4e99805866ffe673287f5e4
MoGamal27 <113600854+MoGamal27@users.noreply.github.com>
2024-09-17 18:57:56 +0300

    chore: connect to mongoodb

---
+++ config/connectDb.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+module.exports = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
